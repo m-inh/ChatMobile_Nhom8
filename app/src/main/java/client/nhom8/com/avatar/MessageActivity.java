d6b8f78702ed815eab2a1be3ec90f3b7a022dc69
bommasterk74 <phamvanthang0910>
2015-12-29 13:14:13 +0700

    Giao diện chát (fix)

--- app/src/main/java/client/nhom8/com/avatar/MessageActivity.java
+++ app/src/main/java/client/nhom8/com/avatar/MessageActivity.java
@@ -5,11 +5,17 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.Image;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,16 +31,17 @@ import modelsnet.BaseMessage;
 /**
  * Created by TooNies1810 on 11/28/15.
  */
-public class MessageActivity extends Activity {
+public class MessageActivity extends ActionBarActivity {
     private static final String TAG = "MessageActivity";
     private EditText edtMessage;
     private Button btnSend;
-
+    private ImageButton imgBtn_send;
     private String uid;
     private String uidFriend;
     private ListView lvMessage;
     private MessageAdapter mAdapter;
-
+    private Toolbar tool_bar;
+    private ImageButton imgBtn_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,7 +53,9 @@ public class MessageActivity extends Activity {
         Toast.makeText(this, "uid: " + uidFriend, Toast.LENGTH_LONG).show();
 
         initViews();
-
+        setSupportActionBar(tool_bar);
+        //getSupportActionBar().setTitle(null);
+        //getSupportActionBar().setDisplayShowTitleEnabled(false);
         //Dang ki broadcast lang nghe tin nhan toi
         IntentFilter filter = new IntentFilter();
         filter.addAction(ListenMessageService.UPDATE_MESSAGE_ACTION);
@@ -82,16 +91,18 @@ public class MessageActivity extends Activity {
     }
 
     private void initViews() {
+        tool_bar = (Toolbar) findViewById(R.id.tool_bar);
         TextView tvFriendName = (TextView) findViewById(R.id.tv_friendname);
         tvFriendName.setText("" + uidFriend);
         edtMessage = (EditText) findViewById(R.id.edt_message);
-        btnSend = (Button) findViewById(R.id.btn_send);
+        //btnSend = (Button) findViewById(R.id.btn_send);
+        imgBtn_send = (ImageButton) findViewById(R.id.imgBtn_send);
         lvMessage = (ListView) findViewById(R.id.lv_message);
         mAdapter = new MessageAdapter(this);
 
         lvMessage.setAdapter(mAdapter);
 
-        btnSend.setOnClickListener(new View.OnClickListener() {
+        imgBtn_send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String contentSms = edtMessage.getText().toString();
@@ -111,6 +122,13 @@ public class MessageActivity extends Activity {
                 }
             }
         });
+        imgBtn_back = (ImageButton) findViewById(R.id.imgBtn_back);
+        imgBtn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MessageActivity.this, "AAAAAAAAAAA", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void sendMessage(String uidFriend, String contentSms) {
@@ -124,4 +142,26 @@ public class MessageActivity extends Activity {
             e.printStackTrace();
         }
     }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_back) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
